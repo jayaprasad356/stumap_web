17951da567f793b4e17c1acebf68a229825a4f45
divaharsha <divakarvan03@gmail.com>
2023-02-22 11:49:57 +0530

    userlist  api updated

--- api/userlist.php
+++ api/userlist.php
@@ -11,22 +11,39 @@ include_once('../includes/crud.php');
 
 $db = new Database();
 $db->connect();
+
 if (empty($_POST['user_id'])) {
     $response['success'] = false;
     $response['message'] = "User Id is Empty";
     print_r(json_encode($response));
     return false;
 }
-$user_id = $db->escapeString($_POST['user_id']);
 
-$sql = "SELECT * FROM users WHERE id != '$user_id' AND id NOT IN (SELECT friend_id FROM friends WHERE user_id = '$user_id')";
+$user_id = $db->escapeString($_POST['user_id']);
+$search = isset($_POST['search']) ? $db->escapeString($_POST['search']) : '';
+if (!empty($_POST['search'])) {
+    $sql = "SELECT * FROM users WHERE id != '$user_id' AND id NOT IN (SELECT friend_id FROM friends WHERE user_id = '$user_id') AND (name like '%" . $search . "%' OR mobile like '%" . $search . "%')";
+} else {
+    $sql = "SELECT * FROM users WHERE id != '$user_id' AND id NOT IN (SELECT friend_id FROM friends WHERE user_id = '$user_id')";
+}
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 if ($num >= 1) {
+    foreach ($res as $row) {
+        $temp['id'] = $row['id'];
+        $temp['name'] = $row['name'];
+        $temp['email'] = $row['email'];
+        $temp['mobile'] = $row['mobile'];
+        $temp['password'] = $row['password'];
+        $temp['latitude'] = $row['latitude'];
+        $temp['longtitude'] = $row['longtitude'];
+        $rows[] = $temp;
+        
+    }
     $response['success'] = true;
-    $response['message'] = "Users Listed Successfully";
-    $response['data'] = $res;
+    $response['message'] = "Users listed Successfully";
+    $response['data'] = $rows;
     print_r(json_encode($response));
 
 }else{
